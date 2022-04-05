d05025c165523f58baa87722e71cad902f0b0340
amrahmed242 <amrahmedrashad1@gmail.com>
2022-04-05 23:19:54 +0200

    first commit

---
+++ bin/algorithms_in_dart.dart
@@ -0,0 +1,4 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+  print('this repo aims for implementing most common algorithms in dart lang!');
+}
---
+++ lib/binary_search/binary_search.dart
@@ -0,0 +1,15 @@
+int? binarySearch({required List<num> array, required num value}) {
+  var mid = 0;
+  var startIndex = 0;
+  var endIndex = array.length - 1;
+  while (startIndex <= endIndex) {
+    mid = ((startIndex + endIndex) / 2).floor();
+    if (array[mid] == value) {
+      return mid;
+    } else if (value > array[mid]) {
+      startIndex = mid;
+    } else {
+      endIndex = mid;
+    }
+  }
+}
